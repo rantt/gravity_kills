cf818c781e234709f2dce6efe983cd8bf751f9b2
Dave Canam <david.canam@gmail.com>
2014-05-18 14:40:13 -0300

    removed gravity_kills copy

--- gravity_kills/js/old/game.js
+++
@@ -1,8 +0,0 @@
-var game = new Phaser.Game(w, h, Phaser.AUTO, 'game');
-
-game.state.add('Boot', Game.Boot);
-game.state.add('Load', Game.Load);
-game.state.add('Menu', Game.Menu);
-game.state.add('Play', Game.Play);
-
-game.state.start('Boot');
